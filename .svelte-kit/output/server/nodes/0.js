25e3f55216d9dcce3809314242ae53ae30828e1d
Capomagico <matty.cap@live.it>
2025-12-27 14:53:18 +0100

    Style: Increase navbar gap to 2rem and reduce weight to 200

--- .svelte-kit/output/server/nodes/0.js
+++ .svelte-kit/output/server/nodes/0.js
@@ -5,6 +5,6 @@ let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
 export { universal };
 export const universal_id = "src/routes/+layout.js";
-export const imports = ["_app/immutable/nodes/0.BXkmfHhi.js","_app/immutable/chunks/DxolTw8k.js","_app/immutable/chunks/DzQrzwyc.js","_app/immutable/chunks/NOZ7Rk1I.js","_app/immutable/chunks/BAQOzU1q.js","_app/immutable/chunks/DrA_DYd2.js","_app/immutable/chunks/BUApaBEI.js","_app/immutable/chunks/B-FJI2Ga.js","_app/immutable/chunks/Dso5EaVA.js"];
+export const imports = ["_app/immutable/nodes/0.BuA_jldO.js","_app/immutable/chunks/DxolTw8k.js","_app/immutable/chunks/DzQrzwyc.js","_app/immutable/chunks/NOZ7Rk1I.js","_app/immutable/chunks/BAQOzU1q.js","_app/immutable/chunks/BudogJKO.js","_app/immutable/chunks/BUApaBEI.js","_app/immutable/chunks/C_0J-Se-.js","_app/immutable/chunks/Dso5EaVA.js"];
 export const stylesheets = ["_app/immutable/assets/0.C5ohE40y.css"];
 export const fonts = [];
--- .svelte-kit/output/server/nodes/2.js
+++ .svelte-kit/output/server/nodes/2.js
@@ -3,6 +3,6 @@
 export const index = 2;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.Co_AMcGW.js","_app/immutable/chunks/DxolTw8k.js","_app/immutable/chunks/DzQrzwyc.js","_app/immutable/chunks/DZV18mLI.js","_app/immutable/chunks/BAQOzU1q.js","_app/immutable/chunks/NOZ7Rk1I.js","_app/immutable/chunks/Se_5M871.js","_app/immutable/chunks/B-FJI2Ga.js"];
+export const imports = ["_app/immutable/nodes/2.m1k8qFOH.js","_app/immutable/chunks/DxolTw8k.js","_app/immutable/chunks/DzQrzwyc.js","_app/immutable/chunks/DZV18mLI.js","_app/immutable/chunks/BAQOzU1q.js","_app/immutable/chunks/NOZ7Rk1I.js","_app/immutable/chunks/Se_5M871.js","_app/immutable/chunks/C_0J-Se-.js"];
 export const stylesheets = ["_app/immutable/assets/2.j6-HfDvd.css"];
 export const fonts = [];
