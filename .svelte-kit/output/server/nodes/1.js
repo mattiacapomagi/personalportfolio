faa17063281bb028292bd532c0efdc06d82c1c1c
Capomagico <matty.cap@live.it>
2025-12-27 14:21:51 +0100

    Style: Set navbar links to Light weight (300)

--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.dRTz8q_O.js","_app/immutable/chunks/DxolTw8k.js","_app/immutable/chunks/DzQrzwyc.js","_app/immutable/chunks/DEWM84os.js","_app/immutable/chunks/BUApaBEI.js","_app/immutable/chunks/Bl4_TwSA.js","_app/immutable/chunks/Dso5EaVA.js"];
+export const imports = ["_app/immutable/nodes/1.nUtcp2CK.js","_app/immutable/chunks/DxolTw8k.js","_app/immutable/chunks/DzQrzwyc.js","_app/immutable/chunks/DrA_DYd2.js","_app/immutable/chunks/BUApaBEI.js","_app/immutable/chunks/B-FJI2Ga.js","_app/immutable/chunks/Dso5EaVA.js"];
 export const stylesheets = [];
 export const fonts = [];
