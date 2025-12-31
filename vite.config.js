6e0fbdd7cb9606e5a20a46b5137670149b9dfc5a
Capomagico <matty.cap@live.it>
2025-12-31 19:18:18 +0100

    fix: Restored missing imagetools import in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { imagetools } from 'vite-imagetools';
 import previewGenerator from './vite-plugin-preview-generator.js';
 
 export default defineConfig({
